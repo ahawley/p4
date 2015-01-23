1c519d9c166a9c84eacfeb32d66850531a84edb9
ahawley <ahawley@mxschool.edu>
2015-01-23 10:25:22 -0500

    Added Read Me and index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<h1>Hello Mr. Torres*</h1>
+<h6>*If you aren't Mr. Torres you probably are on the wrong page</h6>
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus dictum vestibulum urna id porttitor. Vestibulum sollicitudin eros pretium nunc efficitur volutpat. Pellentesque eu tortor eu erat viverra egestas a sit amet nulla. Duis vel purus at lorem ornare volutpat. Quisque quis est ultrices, aliquet felis sed, aliquam elit. Pellentesque semper, nisl ut elementum accumsan, lectus sem lobortis neque, vitae facilisis purus lacus et ligula. Donec arcu tellus, porta tempus tristique quis, aliquam sit amet ligula. Aenean sagittis semper tellus, quis auctor lorem imperdiet in. Interdum et malesuada fames ac ante ipsum primis in faucibus. Suspendisse non finibus libero. Proin id aliquet ex. Sed eleifend sapien vitae libero aliquam, in hendrerit ex pulvinar. Integer eleifend porta nibh id dignissim.</p>
+<img src="dodgetocat.jpg"/>
+<p></p>
+<a href=https://www.mxschool.edu/>The best school in the world</a>
+<ol>
+    <li>This</li>
+    <li>is</li>
+    <li>a</li>
+    <li>list</li>
+</ol>
